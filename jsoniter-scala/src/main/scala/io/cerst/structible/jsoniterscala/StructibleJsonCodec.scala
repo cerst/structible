78f904dd6f67d8e822d2f81f7417b4f383f4eb0e
Constantin Gerstberger <constantin.gerstberger@xaasify.com>
2018-07-16 13:32:24 +0200

    add uuid json-codec

--- jsoniter-scala/src/main/scala/io/cerst/structible/jsoniterscala/StructibleJsonCodec.scala
+++ jsoniter-scala/src/main/scala/io/cerst/structible/jsoniterscala/StructibleJsonCodec.scala
@@ -1,6 +1,12 @@
 package io.cerst.structible.jsoniterscala
 
-import com.github.plokhotnyuk.jsoniter_scala.core.{JsonCodec, JsonReader, JsonWriter}
+import java.util.UUID
+
+import com.github.plokhotnyuk.jsoniter_scala.core.{
+  JsonCodec,
+  JsonReader,
+  JsonWriter
+}
 import io.cerst.structible.Structible
 
 object StructibleJsonCodec {
@@ -294,18 +300,14 @@ object StructibleJsonCodec {
     new JsonCodec[R] {
 
       override def decodeValue(in: JsonReader, default: R): R = {
-        if (in isNextToken 'n') {
-          in.readNullOrError(default, "expected String value or null")
-        } else {
-          in.rollbackToken()
-          // TODO: null
-          val c = in.readString(null)
-          structible constructSafe c match {
-            case Left(error) =>
-              in decodeError error
-            case Right(r) =>
-              r
-          }
+        val c = Option(in.readString(default = null))
+        c map structible.constructSafe match {
+          case None =>
+            default
+          case Some(Left(error)) =>
+            in decodeError error
+          case Some(Right(r)) =>
+            r
         }
       }
 
@@ -321,6 +323,50 @@ object StructibleJsonCodec {
 
       override def decodeKey(in: JsonReader): R = {
         val c = in.readKeyAsString()
+        structible constructSafe c match {
+          case Left(error) =>
+            in decodeError error
+          case Right(r) =>
+            r
+        }
+      }
+
+      override def encodeKey(r: R, out: JsonWriter): Unit = {
+        out writeKey structible.destruct(r)
+      }
+    }
+  }
+
+  // ==========================================================================
+  // UUID
+  // ==========================================================================
+  def uuid[R](implicit structible: Structible[UUID, R]): JsonCodec[R] = {
+    new JsonCodec[R] {
+
+      override def decodeValue(in: JsonReader, default: R): R = {
+        val c = Option(in.readUUID(default = null))
+        c map structible.constructSafe match {
+          case None =>
+            default
+          case Some(Left(error)) =>
+            in decodeError error
+          case Some(Right(r)) =>
+            r
+        }
+      }
+
+      override def encodeValue(r: R, out: JsonWriter): Unit = {
+        if (r != null) {
+          out writeVal structible.destruct(r)
+        } else {
+          out.writeNull()
+        }
+      }
+
+      override def nullValue: R = null.asInstanceOf[R]
+
+      override def decodeKey(in: JsonReader): R = {
+        val c = in.readKeyAsUUID()
         structible constructSafe c match {
           case Left(error) =>
             in decodeError error
